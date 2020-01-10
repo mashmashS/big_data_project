96f231c9677934e98ca348817f712b4f7dcb11de
dianagut1987 <48692892+dianagut1987@users.noreply.github.com>
2020-01-10 14:48:35 +0200

    Update 2.1 clustering between to first article and the last article 2.py

--- 2.1 clustering between to first article and the last article 2.py
+++ 2.1 clustering between to first article and the last article 2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import seaborn as sns; sns.set()
 import pandas as pd
 from sklearn.cluster import KMeans
@@ -17,16 +11,10 @@ x = sd.iloc[:, [1,2]].values
 print(x)
 
 
-kmeans3 = KMeans(n_clusters=3)
-y_kmeans3 = kmeans3.fit_predict(x)
-print(y_kmeans3)
+kmeans4 = KMeans(n_clusters=4)
+y_kmeans4 = kmeans4.fit_predict(x)
+print(y_kmeans4)
 kmeans3.cluster_centers_
 
-plt.scatter(x[:,0],x[:,1],c=y_kmeans3,cmap='rainbow')
-
-
-# In[ ]:
-
-
-
+plt.scatter(x[:,0],x[:,1],c=y_kmeans4,cmap='rainbow')
 
