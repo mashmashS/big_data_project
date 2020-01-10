ccf7f05512438a952666e9301121fb6ef39990f8
dianagut1987 <48692892+dianagut1987@users.noreply.github.com>
2020-01-10 11:33:55 +0200

    חישובים בסיסיים 1.1

---
+++ 1.1 חישובים בסיסיים .py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from apyori import apriori
+
+#sum all of the columns
+store_data = pd.read_csv('D:\\Datasets\\NIPS_1987-2015.csv')
+#sum every column
+sumCol=store_data.sum()
+print(sumCol)
+sumAll=np.sum(sumCol)
+print(sumAll)
+#a=np.squeeze(np.asarray(store_data))
+#find the rows
+x=np.sum(store_data,axis=1)
+#print the rows
+print(x)
+#find the max
+print(max(x))
+print(min(x))
+
+
+# In[ ]:
+
+
+
+
