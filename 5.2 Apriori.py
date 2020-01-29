4e9fe14d5a33bcc7cc6d64966fb7df3d6f29dad2
dianagut1987 <48692892+dianagut1987@users.noreply.github.com>
2020-01-29 12:09:29 +0200

    Update 5.2 Apriori.py

--- 5.2 Apriori.py
+++ 5.2 Apriori.py
@@ -28,15 +28,111 @@ print(association_results[0])
 print(association_results[1])
 print(association_results[2])
 
+פלט:
+    
+   Unnamed: 0  abalone  abbeel  abbott  abbreviate  abbreviated  abc  abeles  \
+0    2015_374  abalone     NaN     NaN         NaN          NaN  NaN     NaN   
+1    2015_375      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+2    2015_376      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+3    2015_377      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+4    2015_378      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+5    2015_379      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+6    2015_380      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+7    2015_381      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+8    2015_382      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+9    2015_383      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+10   2015_384      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+11   2015_385      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+12   2015_386      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+13   2015_387      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+14   2015_388      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+15   2015_389      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+16   2015_390      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+17   2015_391      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+18   2015_392      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+19   2015_393      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+20   2015_394      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+21   2015_395      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+22   2015_396      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+23   2015_397      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+24   2015_398      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+25   2015_399      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+26   2015_400      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+27   2015_401      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+28   2015_402      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
+29   2015_403      NaN     NaN     NaN         NaN          NaN  NaN     NaN   
 
-# In[ ]:
+    abernethy  abilistic  ...  zhou  zhu  zien  zilberstein zones  zoo zoom  \
+0         NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+1         NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+2         NaN        NaN  ...  zhou  NaN   NaN          NaN   NaN  NaN  NaN   
+3         NaN        NaN  ...  zhou  NaN   NaN          NaN   NaN  NaN  NaN   
+4         NaN        NaN  ...  zhou  NaN   NaN          NaN   NaN  NaN  NaN   
+5         NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+6         NaN        NaN  ...   NaN  NaN  zien          NaN   NaN  NaN  NaN   
+7         NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+8         NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+9         NaN        NaN  ...  zhou  NaN   NaN          NaN   NaN  NaN  NaN   
+10        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+11        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+12        NaN        NaN  ...  zhou  NaN   NaN          NaN   NaN  NaN  NaN   
+13        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+14        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+15        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+16        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+17        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+18        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+19        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+20  abernethy        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+21        NaN        NaN  ...  zhou  zhu   NaN          NaN   NaN  NaN  NaN   
+22        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+23        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+24  abernethy        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+25        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+26        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+27        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+28  abernethy        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
+29        NaN        NaN  ...   NaN  NaN   NaN          NaN   NaN  NaN  NaN   
 
+    zou zoubin  zurich  
+0   NaN    NaN     NaN  
+1   NaN    NaN     NaN  
+2   NaN    NaN     NaN  
+3   NaN    NaN     NaN  
+4   NaN    NaN     NaN  
+5   NaN    NaN     NaN  
+6   NaN    NaN     NaN  
+7   NaN    NaN     NaN  
+8   NaN    NaN     NaN  
+9   NaN    NaN     NaN  
+10  NaN    NaN     NaN  
+11  NaN    NaN     NaN  
+12  NaN    NaN     NaN  
+13  NaN    NaN     NaN  
+14  NaN    NaN     NaN  
+15  NaN    NaN     NaN  
+16  NaN    NaN     NaN  
+17  NaN    NaN     NaN  
+18  NaN    NaN     NaN  
+19  NaN    NaN     NaN  
+20  NaN    NaN     NaN  
+21  NaN    NaN     NaN  
+22  NaN    NaN     NaN  
+23  NaN    NaN     NaN  
+24  NaN    NaN     NaN  
+25  NaN    NaN     NaN  
+26  NaN    NaN     NaN  
+27  NaN    NaN     NaN  
+28  NaN    NaN     NaN  
+29  NaN    NaN     NaN  
 
-
-
-
-# In[ ]:
-
-
+[30 rows x 11464 columns]
+[['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'binary', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'bility', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'big', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'binary', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'biostatistics'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'big', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'biometrika', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'binary', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'binomial', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'big', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'bing', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'biometrika', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'binary', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'big', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'big', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'binary', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'bins', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'big', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'biometrika', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan', 'nan']]
+/n
+length: 3
+RelationRecord(items=frozenset({'big', 'bins'}), support=0.034482758620689655, ordered_statistics=[OrderedStatistic(items_base=frozenset({'big'}), items_add=frozenset({'bins'}), confidence=0.16666666666666666, lift=4.833333333333333), OrderedStatistic(items_base=frozenset({'bins'}), items_add=frozenset({'big'}), confidence=1.0, lift=4.833333333333333)])
+RelationRecord(items=frozenset({'binary', 'binomial'}), support=0.034482758620689655, ordered_statistics=[OrderedStatistic(items_base=frozenset({'binary'}), items_add=frozenset({'binomial'}), confidence=0.19999999999999998, lift=5.8), OrderedStatistic(items_base=frozenset({'binomial'}), items_add=frozenset({'binary'}), confidence=1.0, lift=5.8)])
+RelationRecord(items=frozenset({'binary', 'bins'}), support=0.034482758620689655, ordered_statistics=[OrderedStatistic(items_base=frozenset({'binary'}), items_add=frozenset({'bins'}), confidence=0.19999999999999998, lift=5.8), OrderedStatistic(items_base=frozenset({'bins'}), items_add=frozenset({'binary'}), confidence=1.0, lift=5.8)])
+​
 
 
